d8ca2656e89f971dedbb345836569a85c058c295
Marcio de Almeida Rosa <marcio.almeida.rosa@gmil.com>
2023-04-30 11:34:53 -0300

    Ajustando estruturas de pastas

---
+++ Arvore-Binaria/ArvoreBinariaSimples/ArvoreBinariaSimplesTest/BaseArvoreComandoTests.cs
@@ -0,0 +1,18 @@
+namespace ArvoreBinariiaSimplesTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void AdicionandoNoRaizComSucesso()
+    {
+        //Arrange
+        var comando = new AdicionarNovoItemArvoreComando();
+
+
+        //Act
+        var result = comando.Executar(new No());
+
+        //Assert
+
+    }
+}
