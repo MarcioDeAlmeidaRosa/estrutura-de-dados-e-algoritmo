3fcb7c2178853dd03dd833ab438c3635fa893e6d
Marcio de Almeida Rosa <marcio.almeida.rosa@gmil.com>
2023-05-10 21:38:53 -0300

    Criando estrutura inicial de uma árvore red-black

---
+++ Arvores-Binaria/ArvoceBinariaRubroNegra/ArvoceBinariaRubroNegra/No.cs
@@ -0,0 +1,16 @@
+using ExecutorGenerico;
+
+namespace ArvoceBinariaRubroNegra;
+
+public class No: BaseNo
+{
+    public int? Pai { get; set; }
+
+    public int Valor { get; set; }
+
+    public No? Esquerdo { get; set; }
+
+    public No? Direito { get; set; }
+
+    public Tipo Tipo { get; set; }
+}
---
+++ Arvores-Binaria/ArvoceBinariaRubroNegra/ArvoceBinariaRubroNegra/Tipo.cs
@@ -0,0 +1,8 @@
+namespace ArvoceBinariaRubroNegra;
+
+[Flags]
+public enum Tipo
+{
+    Preto = 1,
+    Vermelho = 2,
+}
